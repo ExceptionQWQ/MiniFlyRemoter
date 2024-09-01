eac5ea035b5833b5a2c4bca3ce3300a7d3c67c6a
BusyBox <2894415997@qq.com>
2024-09-02 00:33:00 +0800

    创建工程

---
+++ Core/Src/freertos.c
@@ -0,0 +1,205 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include "adc.h"
+#include "stdio.h"
+#include "string.h"
+#include "usbd_cdc_if.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+
+__attribute__((aligned(32))) uint16_t adc_value[5];
+
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+osThreadId defaultTaskHandle;
+osThreadId KeyScanHandle;
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void const * argument);
+void KeyScanTask(void const * argument);
+
+extern void MX_USB_DEVICE_Init(void);
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 512);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* definition and creation of KeyScan */
+  osThreadDef(KeyScan, KeyScanTask, osPriorityNormal, 0, 128);
+  KeyScanHandle = osThreadCreate(osThread(KeyScan), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const * argument)
+{
+  /* init code for USB_DEVICE */
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN StartDefaultTask */
+
+
+  int cnt = 0;
+
+  /* Infinite loop */
+  for(;;)
+  {
+      char buff[64];
+      snprintf(buff, 64, "adc_value:%d %d %d %d %d\r\n", adc_value[0], adc_value[1], adc_value[2], adc_value[3], adc_value[4]);
+      while (USBD_BUSY == CDC_Transmit_FS(buff, strlen(buff))) {}
+
+      HAL_ADC_Start_DMA(&hadc1, adc_value, 5);
+
+      HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+
+    osDelay(100);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_KeyScanTask */
+/**
+* @brief Function implementing the KeyScan thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_KeyScanTask */
+void KeyScanTask(void const * argument)
+{
+  /* USER CODE BEGIN KeyScanTask */
+  /* Infinite loop */
+  for(;;)
+  {
+      int key_left = !HAL_GPIO_ReadPin(KEY_L_GPIO_Port, KEY_L_Pin);
+      int key_right = !HAL_GPIO_ReadPin(KEY_R_GPIO_Port, KEY_R_Pin);
+      int key_stick1 = !HAL_GPIO_ReadPin(KEY_STICK1_GPIO_Port, KEY_STICK1_Pin);
+      int key_stick2 = !HAL_GPIO_ReadPin(KEY_STICK2_GPIO_Port, KEY_STICK2_Pin);
+
+      if (key_left || key_right || key_stick1 || key_stick2) {
+          HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_SET);
+      } else {
+          HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_RESET);
+      }
+
+    osDelay(1);
+  }
+  /* USER CODE END KeyScanTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+    if (hadc->Instance == ADC1) {
+
+        HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+    }
+}
+
+
+
+/* USER CODE END Application */
+
---
+++ cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/DependInfo.cmake
@@ -0,0 +1,113 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Startup/startup_stm32f103c8tx.s" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Startup/startup_stm32f103c8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../USB_DEVICE/App"
+  "../USB_DEVICE/Target"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/adc.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/adc.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/dma.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/dma.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/freertos.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/freertos.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/gpio.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/gpio.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/main.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/main.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/stm32f1xx_hal_msp.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/stm32f1xx_hal_timebase_tim.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/stm32f1xx_hal_timebase_tim.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/stm32f1xx_it.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/syscalls.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/syscalls.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/sysmem.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/sysmem.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Core/Src/system_stm32f1xx.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_adc.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_adc.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_adc_ex.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_adc_ex.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd_ex.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd_ex.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/list.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/USB_DEVICE/App/usb_device.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/USB_DEVICE/App/usb_device.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/USB_DEVICE/App/usbd_cdc_if.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/USB_DEVICE/App/usbd_cdc_if.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/USB_DEVICE/App/usbd_desc.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/USB_DEVICE/App/usbd_desc.c.obj"
+  "/Users/mac/Desktop/MiniFlyRemoter/USB_DEVICE/Target/usbd_conf.c" "/Users/mac/Desktop/MiniFlyRemoter/cmake-build-debug/CMakeFiles/MiniFlyRemoter.elf.dir/USB_DEVICE/Target/usbd_conf.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../USB_DEVICE/App"
+  "../USB_DEVICE/Target"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
